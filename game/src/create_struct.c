b845ca8bac15aec5c224dbcfa31f032d27146400
Texetomg <texetomg@gmail.com>
2019-04-02 17:54:19 +0300

    fix parser

--- game/src/create_struct.c
+++ game/src/create_struct.c
@@ -6,7 +6,7 @@
 /*   By: bfalmer- <bfalmer-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/27 20:10:38 by thorker           #+#    #+#             */
-/*   Updated: 2019/04/01 17:53:40 by bfalmer-         ###   ########.fr       */
+/*   Updated: 2019/04/02 17:48:48 by bfalmer-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,13 +35,19 @@ t_game	*create_struct(void)
 		check_error_n_exit(1,"malloc error");
 	init_sdl(game);
 	gettimeofday(&game->time, NULL);
-	game->sectors = (t_sector*)malloc(sizeof(t_sector));
 	game->points_cam = (vec2*)malloc(sizeof(vec2) * 3);
 	read_map("src/map", game);
-	*(game->sectors->neighbors) = 0xAA0000;
-	*(game->sectors->neighbors + 1) = 0xAA00;
-	*(game->sectors->neighbors + 2) = 0xAA;
-	game->player.pos.x = 0;
+	/*
+	ft_putnbrln(*((game->sectors + 1)->index_points));
+	ft_putnbrln(*((game->sectors + 1)->index_points + 1));
+	ft_putnbrln(*((game->sectors + 1)->index_points + 2));
+	ft_putnbrln(*((game->sectors + 1)->index_points + 3));
+	*/
+	*((game->sectors + 1)->neighbors) = 0xAA0000;
+	*((game->sectors + 1)->neighbors + 1) = 0xAA00;
+	*((game->sectors + 1)->neighbors + 2) = 0xAA;
+	*((game->sectors + 1)->neighbors + 3) = 0xAA;
+	game->player.pos.x = -3;
 	game->player.pos.y = 0;
 	game->player.angle = 0;
 	game->player.curr_sector = 0;
--- game/src/read_map.c
+++ game/src/read_map.c
@@ -6,7 +6,7 @@
 /*   By: bfalmer- <bfalmer-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/27 19:25:52 by bfalmer-          #+#    #+#             */
-/*   Updated: 2019/04/01 17:55:18 by bfalmer-         ###   ########.fr       */
+/*   Updated: 2019/04/02 17:54:01 by bfalmer-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,36 +70,45 @@ static void	define_sector(t_sector *sector, char *line)
 {
 	int		i;
 	int		j;
+	int		z;
 	int		vectors_count;
 	int		wall_count;
 
 	i = 2;
 	j = 0;
+	z = 0;
 	vectors_count = 0;
 	wall_count = 0;
 	sector->ceil = find_number(line, &i);
 	sector->floor = find_number(line, &i);
-	while (line[i] == ' ')
+	i += 2;
+	z = i;
+	while (line[i] && line[i] != '\t')
 	{
+		if (line[i] == ' ')
+			vectors_count++;
 		i++;
-		vectors_count++;
 	}
-	i -= vectors_count;
+	i = i - (i - z);
 	sector->index_points = (int*)malloc(sizeof(int) * vectors_count + 1);
-	while (vectors_count)
+	while (vectors_count + 1)
 	{
 		*(sector->index_points + j) = find_number(line, &i);
 		vectors_count--;
 		j++;
 	}
-	while (line[i] == ' ')
+	z = i;
+	//ft_putnbrln(i);
+	i += 2;
+	while (line[i] && line[i] != '\t')
 	{
-		i++;
-		wall_count++;
+		if (line[i] == ' ')
+			wall_count++;
+		i++;	
 	}
-	i -= wall_count;
+	i = i - (i - z);
 	sector->count_wall = wall_count + 1;
-	ft_putnbr(sector->count_wall);
+	ft_putnbrln(sector->count_wall);
 	sector->neighbors = (int*)malloc(sizeof(int) * sector->count_wall);
 }
 
@@ -119,8 +128,9 @@ void		read_map(char *name, t_game *game)
 
 	game->count_points = count(fd);
 	game->count_sectors = count(fd);
-	
 	if ((game->points = (vec2*)malloc(sizeof(vec2) * game->count_points)) == 0)
+		check_error_n_exit(1, "malloc error");
+	if ((game->sectors = (t_sector*)malloc(sizeof(t_sector) * game->count_sectors)) == 0)
 		check_error_n_exit(1, "malloc error");
 	while ((gnl = (get_next_line(fd, &line))) != 0)
 	{
