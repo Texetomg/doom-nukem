8a9c5b2fb4178d1195c6153d65d462697efe1b23
Texetomg <texetomg@gmail.com>
2019-04-02 18:34:22 +0300

    leha sraka

--- game/src/create_struct.c
+++ game/src/create_struct.c
@@ -6,7 +6,7 @@
 /*   By: bfalmer- <bfalmer-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/27 20:10:38 by thorker           #+#    #+#             */
-/*   Updated: 2019/04/02 17:56:08 by bfalmer-         ###   ########.fr       */
+/*   Updated: 2019/04/02 18:32:51 by bfalmer-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,24 +37,25 @@ t_game	*create_struct(void)
 	gettimeofday(&game->time, NULL);
 	game->points_cam = (vec2*)malloc(sizeof(vec2) * 3);
 	read_map("src/map", game);
+
 	/*
 	ft_putnbrln(*((game->sectors + 1)->index_points));
 	ft_putnbrln(*((game->sectors + 1)->index_points + 1));
 	ft_putnbrln(*((game->sectors + 1)->index_points + 2));
 	ft_putnbrln(*((game->sectors + 1)->index_points + 3));
-	*/
+	
 	*((game->sectors + 1)->neighbors) = 0xAA0000;
 	*((game->sectors + 1)->neighbors + 1) = 0xAA00;
 	*((game->sectors + 1)->neighbors + 2) = 0xAA;
 	*((game->sectors + 1)->neighbors + 3) = 0xAA;
-	game->player.pos.x = -3;
+	*/
+	game->player.pos.x = 0;
 	*(game->sectors->neighbors) = -1;
 	*(game->sectors->neighbors + 1) = 1;
 	*(game->sectors->neighbors + 2) = -1;
 	game->player.pos.y = 0;
 	game->player.pos.z = 0.1;
 	game->player.angle = 0;
-	game->count_sectors = 1;
 	game->player.curr_sector = 0;
 	return (game);
 }
