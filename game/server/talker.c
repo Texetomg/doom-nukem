6b85ee7fe8d2281f0cd7dd5dd750f5164b7d93ce
Texetomg <texetomg@gmail.com>
2019-05-25 13:53:34 +0300

    test

---
+++ game/server/talker.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define SERVERPORT "9034"    // порт для соединения
+
+int main(int argc, char *argv[])
+{
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    int numbytes;
+    int buf;
+    if (argc != 3) {
+        fprintf(stderr,"usage: talker hostname messagen");
+        exit(1);
+    }
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+
+    if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %sn", gai_strerror(rv));
+        return 1;
+    }
+
+    // пробегаемся по результатам и создаём сокет
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("talker: socket");
+            continue;
+        }
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("client: connect");
+            continue;
+        }
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "talker: failed to bind socketn");
+        return 2;
+    }
+    while (1) {
+        if ((numbytes = send(sockfd, argv[2], strlen(argv[2]), 0) == -1)) {
+            perror("talker: sendto");
+            exit(1);
+        }
+        if ((numbytes = recv(sockfd, (int *) &buf, sizeof(buf), 0)) == -1) {
+            perror("recv");
+            exit(1);
+        }
+        freeaddrinfo(servinfo);
+
+        printf("talker: sent %d bytes to %sn", numbytes, argv[1]);
+    }
+    close(sockfd);
+
+    return 0;
+}
